a01dffc3d45240bbb74fd03e364fb555f3f0c789
Ali Dehghani <ali.dehghani.g@gmail.com>
2019-12-12 13:26:47 +0330

    Registering a brand new error controller.
    Signed-off-by: Ali Dehghani <ali.dehghani.g@gmail.com>

--- src/main/java/me/alidg/errors/conf/ServletErrorsAutoConfiguration.java
+++ src/main/java/me/alidg/errors/conf/ServletErrorsAutoConfiguration.java
@@ -8,11 +8,22 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 import static org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type.SERVLET;
+import static org.springframework.http.MediaType.ALL;
 
 /**
  * Encapsulates servlet-specific parts of errors auto-configuration.
@@ -56,5 +67,45 @@ public class ServletErrorsAutoConfiguration {
     public ErrorAttributes errorAttributes(WebErrorHandlers webErrorHandlers,
                                            HttpErrorAttributesAdapter httpErrorAttributesAdapter) {
         return new ServletErrorAttributes(webErrorHandlers, httpErrorAttributesAdapter);
+    }
+
+    /**
+     * Registers a custom {@link ErrorController} to change the default error handling approach.
+     *
+     * @param errorAttributes  Will be used to enrich error responses.
+     * @param serverProperties Will be used to access error related configurations.
+     * @return The custom error controller instance.
+     */
+    @Bean
+    @ConditionalOnBean(WebErrorHandlers.class)
+    public ErrorController customErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties) {
+        return new CustomServletErrorController(errorAttributes, serverProperties.getError());
+    }
+
+    /**
+     * A very simple custom {@link ErrorController} responsible for fixing the status code
+     * issue in Spring Boot 2.2+.
+     */
+    private static class CustomServletErrorController extends BasicErrorController {
+
+        public CustomServletErrorController(ErrorAttributes errorAttributes, ErrorProperties errorProperties) {
+            super(errorAttributes, errorProperties);
+        }
+
+        /**
+         * Since our custom {@link ErrorAttributes} implementation is storing the HTTP status code inside
+         * the HTTP request, we should call the {@link #getStatus(HttpServletRequest)} method after the
+         * call to {@link #getErrorAttributes(HttpServletRequest, boolean)}.
+         *
+         * @param request The current HTTP request.
+         * @return Returns the HTTP response.
+         */
+        @Override
+        @RequestMapping
+        public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+            Map<String, Object> body = getErrorAttributes(request, isIncludeStackTrace(request, ALL));
+            HttpStatus status = getStatus(request);
+            return new ResponseEntity<>(body, status);
+        }
     }
 }
